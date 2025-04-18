a839d19ede6e1b348dbac8363ae3b67262b4ddda
angelvz30 <angelvaz3510@gmail.com>
2025-04-18 09:26:47 -0700

    Initial Commit

---
+++ FitCheck/FitCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitCheckApp.swift
+//  FitCheck
+//
+//  Created by Angel Vazquez on 4/18/25.
+//
+
+import SwiftUI
+
+@main
+struct FitCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
