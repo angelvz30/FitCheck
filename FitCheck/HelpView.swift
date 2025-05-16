5db48db065302d74cabae506e4f622729c9f1c14
angelvz30 <angelvaz3510@gmail.com>
2025-05-16 10:09:32 -0700

    User questions and automated response

--- FitCheck/HelpView.swift
+++ FitCheck/HelpView.swift
@@ -23,7 +23,7 @@ struct HelpView: View {
                     .padding()
                 
                 Button("Ask") {
-                    response = getAutomatedResponse(for: userQuestions)
+                    response = getAutomatedResponse(for: userQuestion)
                     userQuestion = ""
                 }
                 .buttonStyle(PlainButtonStyle())
@@ -41,19 +41,35 @@ struct HelpView: View {
                     }
                     .padding()
                 }
-                    
+                
                 Spacer()
             }
             .padding()
         }
         
-        private func getAutomatedResponse(for questions: [String: String]) -> String {
-            let lowercasedQuestion = userQuestion.lowercased()
+        func getAutomatedResponse(for question: String) -> String {
+            let lowercasedQuestion = question.lowercased()
             
             switch lowercasedQuestion {
-            case"how do i wash my jeans?":
+            case "how do i wash my jeans?":
                 return "turn them inside out, use cold water, and select a gentle cycle with low heat."
+            case "how do i wash my t-shirt?":
+                return "wash in a cold water cycle, use a suitable detergent, and if using a dryer, opt for low heat and remove the shirt while still damp."
+            case "what does this app do?":
+                return "this app helps you create outfits using your own clothes. just snap a photo of an item, and it shows you how to wear it or builds complete outfits for any occasion from your existing wardrobe."
+            case "do i need to upload photos of my clothes?":
+                return "no, but it improves recommendations. you can also choose items from our generic wardrobe templates."
+            case "can I create outfits myself?":
+                return "Yes, you can manually mix and match pieces and save them as custom outfits."
                 
+                
+            default:
+                <#code#>
+            }
+        }
+struct HelpView_Previews: PreviewProvider {
+    static var previews: some View {
+        HelpView()
             }
         }
     }
